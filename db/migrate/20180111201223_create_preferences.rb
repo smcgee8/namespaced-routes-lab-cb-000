a5d358c589000979f9b4cc728305c7e7caa0909e
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-11 20:13:31 +0000

    automatically backed up by learn

--- db/migrate/20180111201223_create_preferences.rb
+++ db/migrate/20180111201223_create_preferences.rb
@@ -3,8 +3,8 @@ class CreatePreferences < ActiveRecord::Migration
     create_table :preferences do |t|
       t.string :artist_sort_order
       t.string :song_sort_order
-      t.bool :allow_create_artists
-      t.bool :allow_create_songs
+      t.boolean :allow_create_artists
+      t.boolean :allow_create_songs
 
       t.timestamps null: false
     end
